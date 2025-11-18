2e3a864250a1885fbccb7ae33d508ead52df6a2e
m4rt1n99 <144900320+m4rt1n99@users.noreply.github.com>
2025-11-18 20:27:11 +0100

    Create homepage

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<div class="block m-auto">
+    <div class="flex flex-col">
+    <h1 class="text-7xl font-normal">SUPER KOČKY!!</h1>
+    <p class="mt-5">Prozkoumej úžasné a skvělé obrázky kočiček z naší rozsáhlé databáze!!</p>
+    <div class="align-center flex justify-center gap-10 mt-10 font-stack-sans font-normal">
+        <a class="button rounded-md text-center px-5 py-2 bg-amber-700 transition duration-200 hover:bg-amber-600" href="/random">Hodnotit</a>
+        <a class="button rounded-md text-center px-5 py-2 outline-1 outline-amber-700 transition duration-200 hover:bg-amber-600" href="/leaderboard">Prozkoumat</a>
+    </div>
+</div></div>
