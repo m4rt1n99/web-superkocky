cee1cca6f81e385420b5c792e99b57c8e53ddbb5
m4rt1n99 <144900320+m4rt1n99@users.noreply.github.com>
2025-11-25 18:46:00 +0100

    create rating post api

---
+++ src/routes/api/rating/+server.ts
@@ -0,0 +1,19 @@
+import { error, json } from '@sveltejs/kit';
+import { supabase } from "$lib/server/supabaseClient";
+import type { RequestHandler } from './$types';
+
+export const POST: RequestHandler = async ({ request }) => {
+    const json_obj = await request.json()
+    const action:string = json_obj.action
+    const arg_id:number = json_obj.arg_id
+    if(!arg_id || !action){
+        return error(400)
+    }
+    if (action != "add" && action != "subtract"){
+        return error(400)
+    }
+
+	await supabase.rpc(action, {arg_id:arg_id})
+
+    return new Response(null, { status: 200 });
+};
