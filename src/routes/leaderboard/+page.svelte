faa6a843b76e2762adbd3b4c634b423380fa42c3
m4rt1n99 <144900320+m4rt1n99@users.noreply.github.com>
2025-11-25 18:45:02 +0100

    create leaderboard page

---
+++ src/routes/leaderboard/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	const { data } = $props<{ data: { data: { id: number; rating: number; url: string }[] } }>();
+</script>
+
+{#each data.data as entry}
+    <div class="w-fit flex justify-center flex-col mb-10">
+        <div class="w-full flex gap-5 justify-end items-center">
+            <span class="text-xl">Rank: #{data.data.indexOf(entry)+1}</span>
+            <span class="text-xl">Score: {entry.rating}</span>
+            <span class="text-sm">ID: #{entry.id}</span>
+        </div>
+        <img src={entry.url} alt="Cat {entry.id}" class="w-[50vh] object-contain">
+    </div>
+{/each}
---
+++ src/routes/leaderboard/+page.ts
@@ -0,0 +1,9 @@
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ fetch }) => {
+  let res = await fetch('/api/leaderboard');
+  let json = await res.json()
+  let data = json.data
+
+  return { data };
+};
