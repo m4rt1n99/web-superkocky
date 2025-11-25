3367cb5a80188e2a81f6a908e0ccd581be242017
m4rt1n99 <144900320+m4rt1n99@users.noreply.github.com>
2025-11-25 18:45:54 +0100

    create leaderboard api

---
+++ src/routes/api/leaderboard/+server.ts
@@ -0,0 +1,16 @@
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { supabase } from "$lib/server/supabaseClient";
+
+export const GET: RequestHandler = async ({ url }) => {
+  let from: number = url.searchParams.get("from") ? Number(url.searchParams.get("from")) : 1;
+  let to: number = url.searchParams.get("to") ? Number(url.searchParams.get("to")) : from+49;
+
+    const data = await supabase
+    .from("image_data")
+    .select('*')
+    .order("rating", {ascending:false})
+    .range(from, to)
+
+    return json(data)
+};
