c4ba3eb92c8cd2109a0fa679ad8168a393e1802c
m4rt1n99 <144900320+m4rt1n99@users.noreply.github.com>
2025-11-26 18:56:16 +0100

    minor visual changes

--- src/routes/random/+page.svelte
+++ src/routes/random/+page.svelte
@@ -24,10 +24,10 @@
 <h1 class="text-2xl">Tvůj náhodný kočičák</h1>
 <img id="cat-img" src={url} alt="Náhodný kočičák" class="mt-5 h-[50vh]" />
 
-<div class="mt-5 flex w-[30%] items-center justify-between">
+<div class="mt-5 flex w-[30%] items-center justify-center gap-15">
 	<button onclick={() => rating("add", id)}
 		><ChevronsUp
-			class="h-20 w-20 rounded-xl border border-neutral-700 p-3 transition duration-150 hover:cursor-pointer hover:bg-amber-700"
+			class="h-20 w-20 rounded-xl border border-neutral-700 p-3 transition duration-150 hover:cursor-pointer hover:bg-sky-700"
 		/></button
 	>
 	<button onclick={renewImage}
@@ -37,7 +37,7 @@
 	>
 	<button onclick={() => rating("subtract", id)}
 		><ChevronsDown
-			class="h-20 w-20 rounded-xl border border-neutral-700 p-3 transition duration-150 hover:cursor-pointer hover:bg-blue-800"
+			class="h-20 w-20 rounded-xl border border-neutral-700 p-3 transition duration-150 hover:cursor-pointer hover:bg-red-700"
 		/></button
 	>
 </div>
