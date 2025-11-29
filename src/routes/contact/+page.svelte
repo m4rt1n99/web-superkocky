0b95ff9fb0942256a7fa7ed97a0aa4908d5ac000
m4rt1n99 <144900320+m4rt1n99@users.noreply.github.com>
2025-11-29 19:04:39 +0100

    Create contact

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,14 @@
+<style>
+a{
+    color: var(--color-red-300);
+    text-decoration: underline;
+}
+</style>
+<div class="min-w-50 min-h-50 bg-neutral-800 rounded-xl p-5 block m-auto hover:drop-shadow-xl hover:drop-shadow-red-300/20 transition delay-0 duration-250 ease-in-out">
+    <h1 class="text-2xl font-normal">Kontakt</h1>
+    <ul class="mt-5">
+        <li>E-mail: <a href="mailto:spam@xtram.cc">spam@xtram.cc</a></li>
+        <li>Github: <a href="https://github.com/m4rt1n99" class="">m4rt1n99</a></li>
+        <li>Repo (pokud veřejné): <a href="https://github.com/m4rt1n99/web-superkocky" class="">superkocky</a></li>
+    </ul>
+</div>
