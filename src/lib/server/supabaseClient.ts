c5b5d3d33c28afeb9b162396dc3ca4849c099bc0
m4rt1n99 <144900320+m4rt1n99@users.noreply.github.com>
2025-11-30 00:38:40 +0100

    minor style changes

--- src/lib/server/supabaseClient.ts
+++ src/lib/server/supabaseClient.ts
@@ -1,4 +1,3 @@
-// src/lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 import { PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { SUPABASE_SECRET_KEY } from '$env/static/private';
