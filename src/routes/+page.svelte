0a0caa8d16bf92a97d4208881c03d526d18bb8ce
m4rt1n99 <144900320+m4rt1n99@users.noreply.github.com>
2025-11-29 21:18:20 +0100

    style: format document

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
 </script>
 
-<div class="block m-auto">
-    <div class="flex flex-col text-center">
-    <h1 class="text-7xl font-normal">SUPER KOČKY!!</h1>
-    <p class="mt-5">Prozkoumej úžasné a skvělé obrázky kočiček z naší rozsáhlé databáze!!</p>
-    <div class="align-center flex justify-center gap-10 mt-10 font-stack-sans font-normal">
-        <a class="button rounded-md text-center px-5 py-2 bg-amber-700 transition duration-200 hover:bg-amber-600" href="/random">Hodnotit</a>
-        <a class="button rounded-md text-center px-5 py-2 outline-1 outline-amber-700 transition duration-200 hover:bg-amber-600" href="/leaderboard">Prozkoumat</a>
-    </div>
-</div></div>
+<div class="m-auto block">
+	<div class="flex flex-col text-center">
+		<h1 class="text-7xl font-normal">SUPER KOČKY!!</h1>
+		<p class="mt-5">Prozkoumej úžasné a skvělé obrázky kočiček z naší rozsáhlé databáze!!</p>
+		<div class="align-center mt-10 flex justify-center gap-10 font-stack-sans font-normal">
+			<a
+				class="button rounded-md bg-amber-700 px-5 py-2 text-center transition duration-200 hover:bg-amber-600"
+				href="/random">Hodnotit</a
+			>
+			<a
+				class="button rounded-md px-5 py-2 text-center outline-1 outline-amber-700 transition duration-200 hover:bg-amber-600"
+				href="/leaderboard">Prozkoumat</a
+			>
+		</div>
+	</div>
+</div>
