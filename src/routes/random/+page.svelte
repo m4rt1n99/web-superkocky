deecc77d7862dffdd464097bf8c34f4f572d1a83
m4rt1n99 <144900320+m4rt1n99@users.noreply.github.com>
2025-11-29 21:21:57 +0100

    move load function from page.ts to document

--- src/routes/random/+page.svelte
+++ src/routes/random/+page.svelte
@@ -1,31 +1,32 @@
 <script lang="ts">
-	const { data } = $props<{ data: string }>();
-	var url = $state(data.url);
-	var id = $state(data.id);
 	import { ChevronsUp, ChevronsDown, Minus } from 'lucide-svelte';
-
+	import { onMount } from 'svelte';
+	let url: string;
+	let id: number;
 	const renewImage = async () => {
 		const res = await fetch('/img/random');
 		const json = await res.json();
 		url = json.url;
-		id = json.id
+		id = json.id;
 	};
 
 	const rating = async (action: string, id: number) => {
 		await fetch('/api/rating', {
 			method: 'POST',
-			body: JSON.stringify({'action': action, 'arg_id': id})
+			body: JSON.stringify({ action: action, arg_id: id })
 		});
-		await renewImage()
+		await renewImage();
 		return;
-	}
+	};
+
+	onMount(renewImage);
 </script>
 
 <h1 class="text-2xl">Tvůj náhodný kočičák</h1>
 <img id="cat-img" src={url} alt="Náhodný kočičák" class="mt-5 h-[50vh]" />
 
 <div class="mt-5 flex w-[30%] items-center justify-center gap-15">
-	<button onclick={() => rating("add", id)}
+	<button onclick={() => rating('add', id)}
 		><ChevronsUp
 			class="h-20 w-20 rounded-xl border border-neutral-700 p-3 transition duration-150 hover:cursor-pointer hover:bg-sky-700"
 		/></button
@@ -35,7 +36,7 @@
 			class="h-20 w-20 rounded-xl border border-neutral-700 p-3 transition duration-150 hover:cursor-pointer hover:bg-neutral-500"
 		/></button
 	>
-	<button onclick={() => rating("subtract", id)}
+	<button onclick={() => rating('subtract', id)}
 		><ChevronsDown
 			class="h-20 w-20 rounded-xl border border-neutral-700 p-3 transition duration-150 hover:cursor-pointer hover:bg-red-700"
 		/></button
